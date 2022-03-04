6175f9784f23cf8add46f1edf3f1fdc415c8b2d9
Abhilash <abhilashmeher1234@gmail.com>
2022-03-04 13:01:23 +0530

    [Group] Modified main progarm

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 
-namespace palindrom
+namespace aproject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            palindrom pa =new palindrom();
+            palindrom pa = new palindrom();
             Console.WriteLine("enter a number to check whether it is palindrom or not");
-            int num=Convert.ToInt32(Console.ReadLine());
+            int num = Convert.ToInt32(Console.ReadLine());
             pa.pal(num);
+            Armstrong ar = new Armstrong();
+            ar.Arm(num);
+            Class1 prm = new Class1();
+            prm.class1(num);
         }
     }
 }
