ea97a3c474a3eae81de1358dde1afac761b65642
chakravarthy <m.chakra473@gmail.com>
2022-03-04 12:17:37 +0530

    [chakra]:added main file to main branch

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            palindrom pa =new palindrom();
+            Console.WriteLine("enter a number to check whether it is palindrom or not");
+            int num=Convert.ToInt32(Console.ReadLine());
+            pa.pal(num);
+        }
+    }
+}
