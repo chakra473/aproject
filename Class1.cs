e9fd4073048e6a58baf5baf655934198a876d743
sanjay <ssah6694@gmail.com>
2022-03-04 12:29:18 +0530

    [sanjay] Added prime

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProj1
+{
+    internal class Class1
+    {
+        public void class1(int num)
+        {
+            int count = 0;
+            for (int i=1; i<=num; i++ )
+            {
+                if (num%i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("Is not a prime number");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
