172bb6b3cfed7a37af7bc9eaf6d0220c85d5fb59
chakravarthy <m.chakra473@gmail.com>
2022-03-04 12:20:45 +0530

    [chakra]:added palindrom file

---
+++ palindrom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    internal class palindrom
+    {
+        public void pal(int num)
+        {
+            int temp, rem, rev = 0;
+            temp = num;
+            while (num > 0)
+            {
+                rem=num% 10;
+                rev = ((rev * 10) + rem);
+                num = num/10;
+            }
+            if (temp==rev)
+            {
+                Console.WriteLine(temp + " is palindrom number");
+            }
+            else
+            {
+                Console.WriteLine(temp + " is not palindrom number");
+            }
+        }
+    }
+}
